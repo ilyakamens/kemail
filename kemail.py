900ae026651f85c75d9fcede67a8d4a9a74dddd3
Ilya Kamens <ilyakamens@gmail.com>
2014-08-25 01:57:52 -0700

    1. Request handler. 2 Python stub class for subdirectory imports

---
+++ kemail.py
@@ -0,0 +1,30 @@
+import webapp2
+import json
+
+from backend.send_grid_email import *
+
+class DefaultHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(open('./frontend/emailform.html', 'r').read())
+
+class EmailHandler(webapp2.RequestHandler):
+    def post(self):
+		
+        # validate data...
+        # if validation passes...
+
+        s_g_email = SendGridEmail(self.request)
+        reply = s_g_email.send_email()
+
+        if reply.code == 200:
+            response = "Your message was sent!"
+        else:
+            response = "We're sorry, but we could not send your message at this time."
+
+
+        self.response.write(response)
+
+application = webapp2.WSGIApplication([
+    ('/', DefaultHandler),
+    ('/email', EmailHandler)
+], debug=True)
