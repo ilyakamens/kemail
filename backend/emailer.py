eec5fa50e6b6dceb355cef82a203985d562649d7
Ilya Kamens <ilyakamens@gmail.com>
2014-08-26 19:40:02 -0700

    User authentication; daily email limits with Memcache tracking; better user flows

--- backend/emailer.py
+++ backend/emailer.py
@@ -2,25 +2,26 @@ import pkgutil, sys, os, re, httplib, urllib2
 
 class Emailer(object):
 
-	@staticmethod
-	def get_dynamic_inst_data():
+	def __init__(self, user_data):
+		self.user_data = user_data
+
+	def get_dynamic_inst_data(self):
 		path = os.path.abspath(os.path.dirname(__file__))   
 		file_names = os.listdir(path)
 		regex = re.compile("_email\.py$", re.IGNORECASE)
 		pruned_file_names = filter(regex.search, file_names)  
-		module_names = Emailer.get_mod_name(pruned_file_names)
+		module_names = self.get_mod_name(pruned_file_names)
 
 		# Create list of (module name, module) pairs for dynamic class instantiation
 		return [(module_name, __import__(module_name, globals(), locals(), ['object'], -1)) for module_name in module_names]
 
-	@staticmethod
-	def send_email(request):
-		dynamic_inst_data = Emailer.get_dynamic_inst_data()
+	def send_email(self, request):
+		dynamic_inst_data = self.get_dynamic_inst_data()
 
 		for module_name, module in dynamic_inst_data:
 			if module_name != 'base_email':
 				# Load class from imported module
-				class_name = Emailer.get_class_name(module_name)
+				class_name = self.get_class_name(module_name)
 				loaded_class = getattr(module, class_name)
 
 				# Create an instance of the class
@@ -31,16 +32,15 @@ class Emailer(object):
 					continue
 
 				if response.code == httplib.OK:
+					self.user_data.increment_emails_sent_today()
 					return True
 
 		return False
 
-	@staticmethod
-	def get_mod_name(file_names):
+	def get_mod_name(self, file_names):
 		return [os.path.splitext(file_name)[0] for file_name in file_names]
 
-	@staticmethod
-	def get_class_name(mod_name):
+	def get_class_name(self, mod_name):
 	    # Return the class name from a plugin name
 	    output = ''
 
--- kemail.py
+++ kemail.py
@@ -1,24 +1,60 @@
 import webapp2
-import json
-import httplib
 
 from backend.emailer import *
+from backend.user_data import *
+from google.appengine.api import users
+
+EMAIL_FORM = 'email_form'
+SEND_SUCCESS = 'send_success'
+SEND_FAIL = 'send_fail'
+LIMIT_REACHED = 'limit_reached'
+LOG_IN = 'log_in'
+
+def get_client_response(file_name, optional_params=()):
+    return open('./frontend/%s.html' % file_name, 'r').read() % optional_params
 
 class DefaultHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(open('./frontend/emailform.html', 'r').read())
+        # Checks for active Google account session
+        user = users.get_current_user()
+
+        if user:
+            user_data = UserData(user)
+            num_emails_left_today = user_data.get_num_emails_left_today()
+
+            if num_emails_left_today > 0:
+                # Hacky way to show 'email' vs. 'emails'
+                plural = 's' if num_emails_left_today != 1 else ''
+                message = get_client_response(EMAIL_FORM, (user.nickname(), num_emails_left_today, plural))
+            else:
+                message = get_client_response(LIMIT_REACHED, (user.nickname()))
+
+            self.response.write(message)
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
 class EmailHandler(webapp2.RequestHandler):
+    def get(self):
+        self.redirect('/')
+
     def post(self):
-		
+
         # validate data...
         # if validation passes...
 
-        if Emailer.send_email(self.request):
-            message = "Your message was sent!"
+        user = users.get_current_user()
+        if user:
+            user_data = UserData(user)
+            if user_data.can_send_emails_today():
+                emailer = Emailer(user_data) 
+                if emailer.send_email(self.request):
+                    message = get_client_response(SEND_SUCCESS)
+                else:
+                    message = get_client_response(SEND_FAIL, (user.nickname()))
+            else:
+                message = get_client_response(LIMIT_REACHED, (user.nickname()))
         else:
-            message = "Your message could not be sent at this time."
-
+            message = get_client_response(LOG_IN)
 
         self.response.write(message)
 
